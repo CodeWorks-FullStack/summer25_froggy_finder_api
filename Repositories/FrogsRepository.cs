1c1b2b72f95bae5639a275d917c7b2d14ee090f5
Jeremy Fowler <jembyfowler@gmail.com>
2025-07-29 12:16:06 -0600

    got frogs 🐸🐸🐸🐸

--- Repositories/FrogsRepository.cs
+++ Repositories/FrogsRepository.cs
@@ -2,8 +2,19 @@ namespace froggy_finder_api.Repositories;
 
 public class FrogsRepository
 {
+  private readonly IDbConnection _db;
+
+  public FrogsRepository(IDbConnection db)
+  {
+    _db = db;
+  }
+
   public List<Frog> GetFrogs()
   {
-    return [];
+    string sql = "SELECT * FROM frogs;";
+
+    List<Frog> frogs = _db.Query<Frog>(sql).ToList();
+
+    return frogs;
   }
 }
